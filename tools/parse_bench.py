afce7097e399b26e424c0cffdf5e44c45f715d33
Daniel Kales <daniel.kales@gmail.com>
2020-11-12 15:32:22 +0100

    added bench parsing script

---
+++ tools/parse_bench.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import sys
+
+SCALING_FACTOR = 3600.0
+
+with open(sys.argv[1], "r") as f:
+    content = f.read()
+
+testruns = content.split("Instance: ")
+if len(testruns) > 1:
+    testruns.pop(0)
+
+for test in testruns:
+    lines = test.splitlines()
+    # first line is instance:
+    print(lines[0])
+    lines.pop(0)
+    # second line is header:
+    print(lines[0])
+    lines.pop(0)
+
+    count = 0
+    keygen,sign,ver,size,ser,deser = 0,0,0,0,0,0
+
+    for line in lines:
+        if len(line.strip()) == 0:
+            continue
+        vals = line.strip().split(",")
+        keygen += int(vals[0])
+        sign += int(vals[1])
+        ver += int(vals[2])
+        size += int(vals[3])
+        ser += int(vals[4])
+        deser += int(vals[5])
+        count += 1
+    
+    keygen = (keygen / SCALING_FACTOR) / count
+    sign = (sign / SCALING_FACTOR) / count
+    ver = (ver / SCALING_FACTOR) / count
+    size = float(size) / count
+    ser = (ser / SCALING_FACTOR) / count
+    deser = (deser / SCALING_FACTOR) / count
+    print("{},{},{},{},{},{}".format(keygen,sign,ver,size,ser,deser))
+
+    
